41b83f8af5cf97decea25554d098fadd929218ca
Atsushi Nakatsugawa <atsushi@moongift.co.jp>
2025-04-10 11:20:42 +0900

    fix: 経路簡易探索クラス構造をdictから変更

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='ekispert',
-  version='0.1.9',
+  version='0.2.0',
   packages=find_packages(),
   install_requires=[
     # 依存パッケージをここに列挙
